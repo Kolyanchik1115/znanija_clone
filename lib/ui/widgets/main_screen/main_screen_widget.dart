060481af60b18654c7b5af6b4fa06d66552cffa1
Sprat <105858796+Kolyanchik1115@users.noreply.github.com>
2023-02-27 13:22:29 +0200

    add main_screen

--- lib/ui/widgets/main_screen/main_screen_widget.dart
+++ lib/ui/widgets/main_screen/main_screen_widget.dart
@@ -1,12 +1,52 @@
 import 'package:flutter/material.dart';
 
-class MainScreenWidget extends StatelessWidget {
-  const MainScreenWidget({super.key});
+class MainScreenWidget extends StatefulWidget {
+  const MainScreenWidget({Key? key}) : super(key: key);
+
+  @override
+  MainScreenWidgetState createState() => MainScreenWidgetState();
+}
+
+class MainScreenWidgetState extends State<MainScreenWidget> {
+  int _selectedTab = 0;
+
+  void onSelectTab(int index) {
+    if (_selectedTab == index) return;
+    setState(() {
+      _selectedTab = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder(
-      color: Colors.red,
+    return Scaffold(
+      appBar: AppBar(),
+      body: IndexedStack(
+        index: _selectedTab,
+        children: [
+          Container(color: Colors.redAccent),
+          Container(color: Colors.greenAccent),
+          Container(color: Colors.blueAccent),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedTab,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Learn',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.question_answer),
+            label: 'Answer',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_box_outlined),
+            label: 'Account',
+          ),
+        ],
+        onTap: onSelectTab,
+      ),
     );
   }
 }
