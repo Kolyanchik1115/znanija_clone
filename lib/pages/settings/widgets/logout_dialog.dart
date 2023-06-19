534a2577792cd0c93b1df35ffaddd41de84f3b1f
Sprat <105858796+Kolyanchik1115@users.noreply.github.com>
2023-06-19 14:53:38 +0300

    fix: change some files and fix auth bugs

--- lib/pages/settings/widgets/logout_dialog.dart
+++ lib/pages/settings/widgets/logout_dialog.dart
@@ -7,83 +7,88 @@ Future<void> showLogoutDialog(BuildContext context) async {
   await showDialog(
     context: context,
     builder: (context) {
-      return AlertDialog(
-        shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.all(
-            Radius.circular(8),
+      return BlocListener<AuthBloc, AuthState>(
+        listener: (context, state) {
+          if (state.status == AuthStatus.initial) {
+            Navigator.of(context, rootNavigator: true).pushNamedAndRemoveUntil(
+              NewUserSplashPage.routeName,
+              (_) => false,
+            );
+          }
+        },
+        child: AlertDialog(
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(
+              Radius.circular(8),
+            ),
           ),
-        ),
-        content: SizedBox(
-          width: 200,
-          height: 200,
-          child: Column(
-            children: [
-              const SizedBox(height: 30),
-              const Text(
-                'Are you sure to logout?',
-                style: TextStyle(fontWeight: FontWeight.bold),
-              ),
-              const SizedBox(height: 20),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  TextButton(
-                    onPressed: () {
-                      context.read<AuthBloc>().add(AuthLogoutEvent());
-                      Navigator.of(context, rootNavigator: true)
-                          .pushNamedAndRemoveUntil(
-                        NewUserSplashPage.routeName,
-                        (_) => false,
-                      );
-                    },
-                    child: Container(
-                      width: 124,
-                      height: 41,
-                      decoration: const BoxDecoration(
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(51),
+          content: SizedBox(
+            width: 200,
+            height: 200,
+            child: Column(
+              children: [
+                const SizedBox(height: 30),
+                const Text(
+                  'Are you sure to logout?',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 20),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    TextButton(
+                      onPressed: () {
+                        context.read<AuthBloc>().add(AuthLogoutEvent());
+                      },
+                      child: Container(
+                        width: 124,
+                        height: 41,
+                        decoration: const BoxDecoration(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(51),
+                          ),
+                          color: Colors.black,
                         ),
-                        color: Colors.black,
-                      ),
-                      child: const Center(
-                        child: Text(
-                          'Yes',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
+                        child: const Center(
+                          child: Text(
+                            'Yes',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
                         ),
                       ),
                     ),
-                  ),
-                  TextButton(
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    child: Container(
-                      width: 84,
-                      height: 41,
-                      decoration: const BoxDecoration(
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(51),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: Container(
+                        width: 84,
+                        height: 41,
+                        decoration: const BoxDecoration(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(51),
+                          ),
                         ),
-                      ),
-                      child: const Center(
-                        child: Text(
-                          'No',
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 16,
+                        child: const Center(
+                          child: Text(
+                            'No',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                            ),
                           ),
                         ),
                       ),
                     ),
-                  ),
-                ],
-              )
-            ],
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       );
